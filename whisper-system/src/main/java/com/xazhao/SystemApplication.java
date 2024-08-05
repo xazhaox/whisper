0c228833f3920655053e8bcb12f1c3d3b0e88dd4
xazhao <xazhao@126.com>
2024-08-05 09:48:40 +0800

    初始化项目

---
+++ whisper-system/src/main/java/com/xazhao/SystemApplication.java
@@ -0,0 +1,13 @@
+package com.xazhao;
+
+/**
+ * @Description Created on 2024/08/05.
+ * @Author xaZhao
+ */
+
+public class SystemApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
