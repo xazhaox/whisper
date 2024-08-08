f8980e39237d34a00d944fb841acc837afbe3b35
xazhao <xazhao@126.com>
2024-08-08 14:41:18 +0800

    调整目录结构，引入Gateway

---
+++ whisper-gateway/src/main/java/com/xazhao/filter/CustomGlobalFilter.java
@@ -0,0 +1,35 @@
+package com.xazhao.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Description Created on 2024/08/07.
+ * @Author xaZhao
+ */
+
+@Slf4j
+@Component
+public class CustomGlobalFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        ServerHttpRequest request = exchange.getRequest();
+
+        log.info("custom global filter");
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
