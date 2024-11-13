4092126cf80f206cbe0f0740952114f779d31a94
xazhao <xazhao@126.com>
2024-11-13 16:46:50 +0800

    🎉 通过脚本修改项目中所有地址相关内容

--- replace_ips.sh
+++ replace_ips.sh
@@ -3,8 +3,8 @@
 # 通过脚本把项目中的所有地址相关内容改成127.0.0.1
 project_dir="."
 
-# 查找所有 yml 文件
-find "$project_dir" -type f -name "*.yml" | while read -r file; do
+# 查找所有 yaml 文件
+find "$project_dir" -type f -name "*.yaml" | while read -r file; do
   # 使用 sed 替换 IP 地址
   sed -i '' 's/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/127.0.0.1/g' "$file"
   sed -i '' 's/r-[a-zA-Z0-9.-]*\.redis\.rds\.aliyuncs\.com/127.0.0.1/g' "$file"
